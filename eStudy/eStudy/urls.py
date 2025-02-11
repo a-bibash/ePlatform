469f51c6f73debc2ade5b92782a22de2fb3f648d
a.bibash <workinprogress.bibash@gmail.com>
2025-02-11 13:21:23 +0545

    Fix models display in custom admin panel

--- eStudy/eStudy/urls.py
+++ eStudy/eStudy/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('main-admin/', admin.site.urls),
     path('', include('courses.urls')),
     path('adminpanel/', include('adminpanel.urls')),
 ]
